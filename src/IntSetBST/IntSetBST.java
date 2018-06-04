ece248e04413d03595831f96413bebddd2388b2e
HANGYU <hans7368@gmail.com>
2018-06-04 23:41:55 +0900

    Delete Node class modifier public in IntSetBST.java

--- src/IntSetBST/IntSetBST.java
+++ src/IntSetBST/IntSetBST.java
@@ -3,47 +3,44 @@ package IntSetBST;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class IntSetBST{
+public class IntSetBST {
 	int maxelems;
 	int maxval;
 	int num;
-	
+
 	Node root;
-	
-	
+
 	public IntSetBST(int maxelems, int maxval) { // constructor
 		this.root = null;
 		this.maxelems = maxelems;
 		this.maxval = maxval;
 		num = 0;
 	}
-	
-	
-	
-	public void insert(int element){
+
+	public void insert(int element) {
 		Node newNode = new Node(element);
-		
-		if(root == null){
+
+		if (root == null) {
 			root = newNode;
 			num++;
 			return;
 		}
-		
+
 		Node current = root;
 		Node parent = null;
-		
-		while(true){
+
+		while (true) {
 			parent = current;
-			if(element<current.data){				
+			if (element < current.data) {
 				current = current.left;
-				if(current==null){
+				if (current == null) {
 					parent.left = newNode;
 					num++;
 					return;
 				}
-			}else{
+			} else {
 				current = current.right;
-				if(current==null){
+				if (current == null) {
 					parent.right = newNode;
 					num++;
 					return;
@@ -51,94 +48,70 @@ public class IntSetBST{
 			}
 		}
 	}
-	
-	
-	
-	
-	public int size(){
+
+	public int size() {
 		return this.num;
 	}
-	
-	
-	
-	
+
 	public void report(ArrayList<Integer> v) {
 		long startTime = System.currentTimeMillis();
-		
+
 		traversal(v, this.root);
-		
+
 		long finishTime = System.currentTimeMillis() - startTime;
-		
+
 		System.out.println("The time elapsed for the 'report' operation is: " + finishTime + "ms");
 	}
-	
-	
-	
-	
+
 	public void traversal(ArrayList<Integer> v, Node r) {
-		if(r != null) {
+		if (r != null) {
 			traversal(v, r.left);
 			v.add(r.data);
 			traversal(v, r.right);
 		}
 		return;
 	}
-	
-	
-	
-	public static void genSets(int maxelems, int maxval) {   // create an instance of a binary search tree
-		
+
+	public static void genSets(int maxelems, int maxval) { // create an instance of a binary search tree
+
 		long startTimeForEntire = System.currentTimeMillis();
-		
-		
-		
-		
+
 		Random rand = new Random();
 		ArrayList<Integer> v = new ArrayList<Integer>();
-		
-		IntSetBST test = new IntSetBST(maxelems,  maxval);
-		
-		
+
+		IntSetBST test = new IntSetBST(maxelems, maxval);
+
 		long startTimeForInsert = System.currentTimeMillis();
-		
-		for(int i=0 ; i<test.maxelems ; i++) {
-			test.insert(rand.nextInt(test.maxval + 1));  //assign random numbers to the created array
+
+		for (int i = 0; i < test.maxelems; i++) {
+			test.insert(rand.nextInt(test.maxval + 1)); // assign random numbers to the created array
 		}
 		long finishTimeForInsert = System.currentTimeMillis() - startTimeForInsert;
 		System.out.println("The time elapsed for the 'insert' operation is: " + finishTimeForInsert + "ms");
-		
-		
-		
+
 		test.report(v);
-		
-		
-				
-		
+
 		long finishTimeForEntire = System.currentTimeMillis() - startTimeForEntire;
 		System.out.println("The time elapsed for the entire operation is: " + finishTimeForEntire + "ms");
-		
+
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		genSets(10000, 1000000);	// í…ŒìŠ¤íŠ¸ìš© set ìƒì„±	
+		genSets(10000, 1000000); // Å×½ºÆ®¿ë set »ý¼º
 	}
 }
 
-
-
-public class Node{
+class Node {
 	int data;
 	Node left;
 	Node right;
-	
-	public Node(){
+
+	public Node() {
 		this.left = null;
 		this.right = null;
 	}
-	
-	public Node(int v){
+
+	public Node(int v) {
 		this.data = v;
 		this.left = null;
 		this.right = null;
