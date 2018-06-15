060813c924f4d9dc90f1845c463f04a4e6d2803f
HANGYU <hans7368@gmail.com>
2018-06-15 21:33:59 +0900

    Edit IntSet.java (add function description), Implement IntSetArr.java, Modify Main.java to test IntSetArr

--- src/IntSet/Main.java
+++ src/IntSet/Main.java
@@ -1,10 +1,34 @@
 package IntSet;
 
+import java.util.Random;
+
 public class Main {
+	static int[] v;
+	static int maxelems;
+	static int maxval;
+	static Random r;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		maxelems = 100000;
+		maxval = Integer.MAX_VALUE;
+		r = new Random();
 
+		long start_time = System.currentTimeMillis();
+		IntSet arraySet = new IntSetArr();
+
+		arraySet.intSetImp(maxelems, maxval);
+
+		while (arraySet.size() < maxelems) {
+			arraySet.insert(r.nextInt(maxval));
+		}
+
+		v = arraySet.report();
+		long end_time = System.currentTimeMillis();
+		long timeDiff = end_time - start_time;
+
+		for (int i = 0; i < v.length; i++) {
+			System.out.println("v[" + i + "] = " + v[i]);
+		}
+		System.out.println("Execution time = " + timeDiff / 1000 + "sec");
 	}
-
 }
--- src/UnitTest/IntSetArrTest.java
+++ src/UnitTest/IntSetArrTest.java
@@ -4,10 +4,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class IntSetArrTest {
+import IntSet.IntSetArr;
+
+public class IntSetArrTest {
+
+	final int bigNum = 10000;
+
+	public IntSetArrTest() {
+
+	}
 
 	@Test
-	void test() {
+	public void testIntSetImp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReport() {
 		fail("Not yet implemented");
 	}
 
